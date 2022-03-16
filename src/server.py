3a403d5bbac97c60bf71cb6f34bc664f43cfc393
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-03-17 00:57:51 +0900

    postまで

--- src/server.py
+++ src/server.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+@app.route('/blog')
+def get_blog():
     return render_template('index.html')
+
+@app.route('/blog',methods=['POST'])
+def post_blog():
+    title = request.form['title']
+    body = request.form['body']
+    return f"タイトルは{title}です。本文は{body}です。"
+    
